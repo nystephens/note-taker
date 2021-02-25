ade4a2cc5f06b91218887c6db8ca4024641afd3e
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-02-25 10:29:25 -0800

    notes and homepage active on server!

--- server.js
+++ server.js
@@ -10,11 +10,20 @@ const PORT = process.env.PORT || 3001;
 // assign express to variable app 
 const app = express();
 
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('*', (req, res) => {
+
+// sets up homepage when server is loaded
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+// sets up response for /notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.listen(PORT, () => {
